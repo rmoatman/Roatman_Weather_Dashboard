567885acf1e0572908a1f681d5d33c2789660494
RMOATMAN <raemarie.oatman@gmail.com>
2021-06-08 12:47:22 -0400

    Added image folder, updated README, cleaned up and commented code

--- Assets/script.js
+++ Assets/script.js
@@ -1,64 +1,66 @@
 // FUNCTION LIST //
-// formSubmitHandler - when submit button is clicked, gets user city and state and combines into one variable for use in API //
-// getUserRepos - Fetches apiUrl and apiUrlUv to get forcasts.  Populates forecast cards.
-// addlocationbutton - //
+// formSubmitHandler - Combines user city and state  into one variable for use in API when submit button is clicked //
+// useStored - Checks to see if location already exists in local storage--if so, does not duplicate //
+// saveToLocalStorage - Adds search locations to local storage array //
+// getLocations - Searches stored in local storage array are added as recent search buttons when page is loaded or refreshed //
+// getUserRepos - Fetches apiUrl and apiUrlUv to get forcasts.  Populates daily and 5-day forecast cards //
+// clearLocalStorage - Clears local storage when button is selected //
 
 
 // VARIABLES //
 var locationarray = [];
+var searchlocation = "";
+var savedlocations = [];
+var wasbuttonclicked = 0;
+
+// User Input Form //
 var usercityEl = document.querySelector("#city");
 var userstateEl = document.querySelector("#state");
 var submitButtonEl = document.querySelector("#fetch-button");
-var citynameEl = document.querySelector(".forecast"); // used to add city name and weather icon to daily forecast card //
+var clearButtonEl = document.querySelector("#clear-button");
+var searchheading1el = document.querySelector("#recent1") // used to hide heading if not in use //
+var searchheading2el = document.querySelector("#recent2") // used to hide heading if not in use //
+
+// Daily Forecast //
+var forecastcardEl = document.querySelector(".forecastcard"); // used to hide daily forecast card //
+var citynameEl = document.querySelector(".forecast");
 var currentdateEl = document.querySelector("#currentdate");
 var temperatureEl = document.querySelector("#temperature");
 var humidityEl = document.querySelector("#humidity");
 var windspeedEl = document.querySelector("#windspeed");
 var uvindexEl = document.querySelector("#uvindex");
-var forecastcardEl = document.querySelector(".forecastcard"); // used to hide daily forecast card //
-var fivedayforecastcardEl = document.querySelector(".fivedayforecastcard");
+
+// 5-Day Forecast Cards
+var fivedayforecastcardEl = document.querySelector(".fivedayforecastcard"); // used to hide 5-day forecast cards //
 var currentdate1El = document.querySelector("#currentdate1");
 var currentdate2El = document.querySelector("#currentdate2");
 var currentdate3El = document.querySelector("#currentdate3");
 var currentdate4El = document.querySelector("#currentdate4");
 var currentdate5El = document.querySelector("#currentdate5");
+
 var weathericon1el = document.querySelector("#weathericon1");
 var weathericon2el = document.querySelector("#weathericon2");
 var weathericon3el = document.querySelector("#weathericon3");
 var weathericon4el = document.querySelector("#weathericon4");
 var weathericon5el = document.querySelector("#weathericon5");
+
 var temperature1El = document.querySelector("#temperature1");
 var temperature2El = document.querySelector("#temperature2");
 var temperature3El = document.querySelector("#temperature3");
 var temperature4El = document.querySelector("#temperature4");
 var temperature5El = document.querySelector("#temperature5");
+
 var windspeed1El = document.querySelector("#windspeed1");
 var windspeed2El = document.querySelector("#windspeed2");
 var windspeed3El = document.querySelector("#windspeed3");
 var windspeed4El = document.querySelector("#windspeed4");
 var windspeed5El = document.querySelector("#windspeed5");
+
 var humidity1el = document.querySelector("#humidity1");
 var humidity2el = document.querySelector("#humidity2");
 var humidity3el = document.querySelector("#humidity3");
 var humidity4el = document.querySelector("#humidity4");
 var humidity5el = document.querySelector("#humidity5");
-var searchheading1el = document.querySelector("#recent1")
-var searchheading2el = document.querySelector("#recent2")
-var searchlocation = "";
-var savedlocations = [];
-var wasbuttonclicked = 0;
-
-
-var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
-var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
-  return new bootstrap.Dropdown(dropdownToggleEl)
-})
-
-
-
-
-
-
 
 
 // FUNCTIONS //
@@ -79,8 +81,16 @@ var formSubmitHandler = function (event) {
 };
 // end of formSubmitHandler //
 
+// Searches for a recently searched location when button is clicked //
+var useStored = function(event) {
+  searchlocation = event.target.getAttribute("value");
+  wasbuttonclicked = 1;
+  getUserRepos(searchlocation);
+}
+// end of useStored //
+
 // Saves searchlocations to local storage //
-function savetolocalstorage(searchlocation) {
+function saveToLocalStorage(searchlocation) {
 
   if (searchlocation !==",") { // Check to see if user entered data //
   
@@ -94,25 +104,21 @@ function savetolocalstorage(searchlocation) {
     localStorage.setItem("locations", JSON.stringify(locationarray));
   };
 };
-// end of savetolocalstorage //
+// end of saveToLocalStorage //
  
-
-
-// Loads cities from local storage
-var getlocations = function() {
+// Loads cities from local storage //
+// This function significantly influenced by Mila Decker's code located at https://github.com/deckiedevs/weather-dashboard/blob/main/assets/js/script.js //
+var getLocations = function() {
   locationarray = JSON.parse(localStorage.getItem("locations"));
 
   if (!locationarray) {
     locationarray = [];
       return false;
-    /*  } else if (locationarray.length > 5) {
-        // saves only the five most recent cities
-        locationarray.shift();
-        }*/
   }
 
   searchheading1el.classList.remove("hide");
   searchheading2el.classList.remove("hide");
+  clearButtonEl.classList.remove("hide");
 
   var savedlocations = document.querySelector("#savedsearchlist");
   var locationslistUl = document.createElement("ul");
@@ -127,13 +133,13 @@ var getlocations = function() {
 
       var sep = locationarray[i].split(",");
       recentcitystate.textContent = sep[0] + ", " + sep[1];
-      locationslistUl.appendChild(recentcitystate);
+      locationslistUl.prepend(recentcitystate);
   }
 
   var searchedcities = document.querySelector(".savedbutton");
-  searchedcities.addEventListener('click', usestored);
+  searchedcities.addEventListener('click', useStored);
 };
-// end of getlocations //
+// end of getLocations //
 
 //Fetch the API that contains today's forecast for the city and state submitted //
 var getUserRepos = function (searchlocation) {
@@ -273,7 +279,7 @@ var getUserRepos = function (searchlocation) {
                     windspeed5El.innerHTML = "Wind:  " + dataUV.daily[5].wind_speed + " MPH";
                     humidity5el.innerHTML = "Humidity:  " + dataUV.daily[5].humidity + "%";
 
-                    savetolocalstorage(searchlocation);
+                    saveToLocalStorage(searchlocation);
                 });
               } 
             });
@@ -285,17 +291,18 @@ var getUserRepos = function (searchlocation) {
 };
 // end of getUserRepos //
 
-// Searches for a recently searched location when button is clicked //
-var usestored = function(event) {
-  searchlocation = event.target.getAttribute("value");
-  wasbuttonclicked = 1;
-  getUserRepos(searchlocation);
+function clearLocalStorage() {
+  window.localStorage.clear();
+  location.reload();
+  clearButtonEl.classList.add("hide")
 }
-// end of usestored //
-
 
 
      // EXECUTION //
-      getlocations();
+      getLocations();
+      submitButtonEl.addEventListener("click", formSubmitHandler);
+      clearButtonEl.addEventListener("click", clearLocalStorage);
 
-      submitButtonEl.addEventListener('click', formSubmitHandler);
+// Last Revised 6/08/21 //
+// Raemarie Oatman raemarie.oatman@gmail.com //
+// Homework Week 6 - 06 Server-Side APIs: Weather Dashboard //
